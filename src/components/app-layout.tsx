19406e5129a5a2685481150010d8c8a2932ee8d9
Yasir Solkar <yasirsolkar2015@gmail.com>
2025-12-16 09:15:29 +0000

    This hydration error occurs because your server renders a sidebar layout

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -17,7 +17,6 @@ import {
   SidebarTrigger,
   SidebarInset,
 } from '@/components/ui/sidebar';
-import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { ForkliftIcon } from './icons/forklift-icon';
